1ed0d04d9ee4d3cd1011eb1c5a79e49cd42cb359
PiKeyAr <PiKeyAr@users.noreply.github.com>
2022-04-24 01:18:19 +0900

    Added IDA script generator and label stripper

--- CommandLine/LabelTool/Program.cs
+++ CommandLine/LabelTool/Program.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using SAModel;
 using SplitTools;
 
 // This tool was made to facilitate label transfer between the X360 and 2004 PC versions of SADX. Code is very rough and not meant for regular use.
 
 namespace LabelTool
 {
-	partial class Program
-	{
+    partial class Program
+    {
         static void Main(string[] args)
         {
             switch (args[args.Length - 1])
             {
+                // Generate script for IDA and DataPointers
+                case "-ida":
+                    LabelGen_IdaAndPointers(args);
+                    return;
                 // Generate labels
                 case "-g":
                     LabelGen_Main(args);
@@ -29,7 +34,124 @@ namespace LabelTool
                 case "-di":
                     LabelDuplicateINI(args);
                     return;
+                // Strip labels from an SAModel file
+                case "-st":
+                    LabelStrip(args);
+                    return;
             }
+        }
+
+        static List<string> addresses;
+        static List<string> names;
+
+        static void WriteDataPointerItem(TextWriter output, string type, string name, string address, bool array = false, string count = "")
+        {
+            if (array)
+               output.WriteLine("DataArray(" + type + ", " + name + ", 0x" + address + ", " + count + ");");
+            else
+               output.WriteLine("DataPointer(" + type + ", " + name + ", 0x" + address +");");
+        }
+
+        static void WriteIdaScriptItem(TextWriter output, string type, string name, string address, bool array = false, string count = "")
+        {
+            if (array)
+            {
+                output.WriteLine("del_items(0x" + address + ");");
+                output.WriteLine("apply_type(0x" + address + ",\"" + type + " " + name + "[" + count + "]" + ";\", 1);");
+                output.WriteLine("set_name(0x" + address + ",\"" + name + "\");");
+            }
+            else
+            {
+                output.WriteLine("del_items(0x" + address + ");");
+                output.WriteLine("apply_type(0x" + address + ",\"" + type + " " + name + ";\", 1);");
+                output.WriteLine("set_name(0x" + address + ",\"" + name + "\");");
+            }
+        }
+
+        // Strips metadata from .sa1lvl, .sa1mdl and .saanim files
+        static void LabelStrip(string[] args)
+        {
+            string outpath = Path.Combine(Path.GetDirectoryName(args[0]), Path.GetFileNameWithoutExtension(args[0]) + "_n" + Path.GetExtension(args[0]));
+            Console.WriteLine("Output path: {0}", outpath);
+            switch (Path.GetExtension(args[0]).ToLowerInvariant())
+            {
+                case ".sa1lvl":
+                case ".sa2lvl":
+                    LandTable lt = LandTable.LoadFromFile(args[0]);
+                    lt.SaveToFile(outpath, lt.Format, true);
+                    break;
+                case ".sa1mdl":
+                case ".sa2mdl":
+                case ".sa2bmdl":
+                    ModelFile mf = new ModelFile(args[0]);
+                    NJS_OBJECT ob = mf.Model;
+                    ModelFile.CreateFile(outpath, ob, null, null, null, new Dictionary<uint, byte[]>(), ob.GetModelFormat(), true);
+                    break;
+                case ".saanim":
+                    NJS_MOTION mot = NJS_MOTION.Load(args[0]);
+                    mot.Save(outpath, true);
+                    break;
+            }
+        }
+
+        // Loads transfer_ida.txt and outputs a header with DataArrays and DataPointers
+        static void LabelGen_IdaAndPointers(string[] args)
+        {
+            bool check = false; // Set to true to check for duplicate names/addresses
+            if (check)
+            {
+                addresses = new List<string>();
+                names = new List<string>();
+            }
+            string[] source = File.ReadAllLines(args[0]);
+            TextWriter output_ida = File.CreateText("transfer_ida.idc");
+            TextWriter output_dp = File.CreateText("transfer_dp.h");
+            output_ida.WriteLine("static main()");
+            output_ida.WriteLine("{");
+            for (int i = 0; i < source.Length; i++)
+            {
+                //Console.WriteLine("Line: {0}", source[i]);
+                string[] div = source[i].Split(',');
+                // Array
+                if (div.Length == 4)
+                {
+                    if (check && (addresses.Contains(div[3]) || names.Contains(div[2])))
+                    {
+                        Console.WriteLine("Duplicate entry: {0} at {1}", div[2], div[3]);
+                        continue;
+                    }
+                    if (check)
+                    {
+                        addresses.Add(div[3]);
+                        names.Add(div[2]);
+                    }
+                    WriteIdaScriptItem(output_ida, div[1], div[2], div[3], true, div[0]);
+                    WriteDataPointerItem(output_dp, div[1], div[2], div[3], true, div[0]);
+                }
+                // Regular
+                else if (div.Length == 3)
+                {
+                    if (check && (addresses.Contains(div[2]) || names.Contains(div[1])))
+                    {
+                        Console.WriteLine("Duplicate entry: {0} at {1}", div[1], div[2]);
+                        continue;
+                    }
+                    if (check)
+                    {
+                        addresses.Add(div[2]);
+                        names.Add(div[1]);
+                    }
+                    WriteIdaScriptItem(output_ida, div[0], div[1], div[2]);
+                    WriteDataPointerItem(output_dp, div[0], div[1], div[2]);
+                }
+                else
+                    Console.WriteLine("Unknown line: {0}", source[i]);
+            }
+            output_ida.WriteLine("}");
+            output_ida.Flush();
+            output_ida.Close();
+            output_dp.Flush();
+            output_dp.Close();
         }
 
         // Builds a label database by comparing two folders containing labelled and unlabelled assets
