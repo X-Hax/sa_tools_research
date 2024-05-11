3594335393d5e7e150af499b147bd3f8c0626c91
PkR <PiKeyAr@users.noreply.github.com>
2024-05-11 21:01:55 +0900

    RecreatePVM: Support PRS and remove double extension

--- CommandLine/RecreatePVM/Program.cs
+++ CommandLine/RecreatePVM/Program.cs
@@ -18,16 +18,30 @@ namespace RecreatePVM
             {
                 Console.WriteLine("This tool builds a PVM from a list of texture names (created by BlockBitTool or split) by recursively searching for each texture in the 'textures' folder.");
                 Console.WriteLine("\nUsage:");
-                Console.WriteLine("RecreatePVM <texturelist.satex> [-q]");
+                Console.WriteLine("RecreatePVM <texturelist.satex> [-q] [-p]");
                 Console.WriteLine("\nSwitches:");
                 Console.WriteLine("-q: When multiple unique textures with the same name are found, pick the first one by default (no prompt)");
+                Console.WriteLine("-p: Compress to PRS");
                 Console.WriteLine("\nPress ENTER to exit.");
                 Console.ReadLine();
                 return;
             }
             bool quick = false;
-            if (args.Length > 1 && args[1] == "-q")
-                quick = true;
+            bool prs = false;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-q":
+                        quick = true;
+                        break;
+                    case "-p":
+                        prs = true;
+                        break;
+
+                }
+            }
             int count = 0;
             string[] files;
             if (Path.GetExtension(args[0]).ToLowerInvariant() == ".satex")
@@ -40,54 +54,63 @@ namespace RecreatePVM
             PuyoFile pvm = new PuyoFile();
             for (int i = 0; i < files.Length; i++)
             {
-                    string[] found = Directory.GetFiles("textures", files[i] + ".pvr", SearchOption.AllDirectories);
-                    if (found.Length == 1)
+                string[] found = Directory.GetFiles("textures", files[i] + ".pvr", SearchOption.AllDirectories);
+                if (found.Length == 1)
+                {
+                    pvm.Entries.Add(new PVMEntry(File.ReadAllBytes(found[0]), Path.GetFileName(files[i])));
+                    Console.WriteLine("{0}: Found {1}", i.ToString(), files[i] + ".pvr");
+                }
+                else if (found.Length > 1)
+                {
+                    if (quick)
                     {
                         pvm.Entries.Add(new PVMEntry(File.ReadAllBytes(found[0]), Path.GetFileName(files[i])));
                         Console.WriteLine("{0}: Found {1}", i.ToString(), files[i] + ".pvr");
                     }
-                    else if (found.Length > 1)
-                    {
-                        if (quick)
-                        {
-                            pvm.Entries.Add(new PVMEntry(File.ReadAllBytes(found[0]), Path.GetFileName(files[i])));
-                            Console.WriteLine("{0}: Found {1}", i.ToString(), files[i] + ".pvr");
-                        }
-                        else
-                        {
-                            List<string> uniqueTexturesFound = new List<string>();
-                            uniqueTexturesFound.Add(found[0]);
-                            for (int l = 1; l < found.Length; l++)
-                            {
-                                if (!isTextureInList(found[l], uniqueTexturesFound))
-                                    uniqueTexturesFound.Add(found[l]);
-                            }
-                            int res = 0;
-                            if (uniqueTexturesFound.Count > 1)
-                            {
-                                Console.WriteLine();
-                                Console.WriteLine("Multiple unique textures found for {0}", files[i]);
-                                for (int z = 0; z < uniqueTexturesFound.Count; z++)
-                                    Console.WriteLine("Texture {0}:{1}", z, uniqueTexturesFound[z]);
-                                Console.WriteLine("Select which texture to use (type 1 or 2 or 3 etc.)");
-                                string result = Console.ReadLine();
-                                bool q = int.TryParse(result, out res);
-                                if (!q)
-                                    Console.WriteLine("Error parsing ID, using the first texture");
-                            }
-                            Console.WriteLine();
-                            pvm.Entries.Add(new PVMEntry(File.ReadAllBytes(uniqueTexturesFound[res]), Path.GetFileNameWithoutExtension(files[i])));
-                            Console.WriteLine("{0}: Found {1}", i.ToString(), uniqueTexturesFound[res]);
-                        }
-                    }
                     else
                     {
-                        pvm.Entries.Add(new PVMEntry(Properties.Resource1.unknown, Path.GetFileName(files[i]) + ".pvr"));
-                        Console.WriteLine("{0}: Missing {1}", i.ToString(), files[i]);
-                        count++;
+                        List<string> uniqueTexturesFound = new List<string>();
+                        uniqueTexturesFound.Add(found[0]);
+                        for (int l = 1; l < found.Length; l++)
+                        {
+                            if (!isTextureInList(found[l], uniqueTexturesFound))
+                                uniqueTexturesFound.Add(found[l]);
+                        }
+                        int res = 0;
+                        if (uniqueTexturesFound.Count > 1)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Multiple unique textures found for {0}", files[i]);
+                            for (int z = 0; z < uniqueTexturesFound.Count; z++)
+                                Console.WriteLine("Texture {0}:{1}", z, uniqueTexturesFound[z]);
+                            Console.WriteLine("Select which texture to use (type 1 or 2 or 3 etc.)");
+                            string result = Console.ReadLine();
+                            bool q = int.TryParse(result, out res);
+                            if (!q)
+                                Console.WriteLine("Error parsing ID, using the first texture");
+                        }
+                        Console.WriteLine();
+                        pvm.Entries.Add(new PVMEntry(File.ReadAllBytes(uniqueTexturesFound[res]), Path.GetFileNameWithoutExtension(files[i])));
+                        Console.WriteLine("{0}: Found {1}", i.ToString(), uniqueTexturesFound[res]);
                     }
+                }
+                else
+                {
+                    pvm.Entries.Add(new PVMEntry(Properties.Resource1.unknown, Path.GetFileName(files[i]) + ".pvr"));
+                    Console.WriteLine("{0}: Missing {1}", i.ToString(), files[i]);
+                    count++;
+                }
             }
-            pvm.Save(Path.ChangeExtension(args[0], ".pvm"));
+            // Remove double extension in SA Tools satex files
+            string outfile = args[0].Replace(".tls", "");
+            if (prs)
+            {
+                Console.WriteLine("Compressing PRS...");
+                byte[] prsdata = FraGag.Compression.Prs.Compress(pvm.GetBytes());
+                File.WriteAllBytes(Path.ChangeExtension(outfile, ".prs"), prsdata);
+            }
+            else
+                pvm.Save(Path.ChangeExtension(outfile, ".pvm"));
             Console.WriteLine("Missing {0} textures of {1}", count, pvm.Entries.Count);
         }
 
