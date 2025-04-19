19b2efd84957d4438ee1cf6539ae3d3d23d1feda
PiKeyAr <pikeyar@pm.me>
2025-04-19 14:19:58 +0900

    ScanAction: Try to find reference models, check action etc., cleanup

--- CommandLine/ScanAction/Program.cs
+++ CommandLine/ScanAction/Program.cs
@@ -11,29 +11,42 @@ namespace ScanAction
     partial class Program
     {
         // This program opens all splitBinary INI files in the folder and attempts to locate actions for all motions.
-        // It also converts SADX death zones to "basicdxmodel" items.
         // If multiple actions are found for a motion, its split type is changed to "actionM" to indicate the requirement of a manual fix.
+        // The output INI file will only contain landtables, models, actions, motions (if an action wasn't found for them), shape motions and texlists.
         private static void Main(string[] args)
         {
+            // Set encoder
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-            string iniPath = Path.Combine(Environment.CurrentDirectory, "ini");
-            string assetPath = Environment.CurrentDirectory;
+            // Set paths
+            string iniPath = Path.Combine(Environment.CurrentDirectory, "ini"); // Location of INI files to parse
+            string assetPath = Environment.CurrentDirectory; // Location of data files
+            // Temporary variables
+            int numparts = 0; // Number of model parts in the motion
+            bool chunk = false; // Whether the reference model is chunk or not
+            // Load INI files           
             string[] iniFiles = Directory.GetFiles(iniPath, "*.ini", SearchOption.TopDirectoryOnly);
+            // Clear screen
+            Console.Clear();
+            Console.Write("\x1b[3J");
             for (int i = 0; i < iniFiles.Length; i++)
             {
-                Console.WriteLine("\nProcessing split data: " + Path.GetFileName(iniFiles[i]));
+                Console.WriteLine("Processing split data: " + Path.GetFileName(iniFiles[i]));
                 // Load INI file
                 IniData iniData = IniSerializer.Deserialize<IniData>(iniFiles[i]);
-                IniData newdata = new IniData { DataFilename = iniData.DataFilename, Game = Game.SADX, ImageBase = iniData.ImageBase, Files = new() };
-                // Load binary file
-                byte[] datafile = File.ReadAllBytes(Path.Combine(assetPath, iniData.DataFilename));
                 // Set default key
                 if (iniData.ImageBase == null)
                     iniData.ImageBase = 0x400000;
+                // Create clone INI data for actions
+                IniData newdata = new IniData { DataFilename = iniData.DataFilename, Game = Game.SADX, ImageBase = iniData.ImageBase, Files = new() };
+                // Load binary file
+                byte[] datafile = File.ReadAllBytes(Path.Combine(assetPath, iniData.DataFilename));
                 foreach (var item in iniData.Files)
                 {
                     switch (item.Value.Type)
                     {
+                        /*
+                        // This doesn't fit the overall purpose of the tool but was useful for SADX decomp purposes. 
+                        // It converts SADX death zone items to "basicdxmodel" items.
                         case "deathzone":
                             List<string> hashes = new List<string>();
                             int num = 0;
@@ -49,6 +62,7 @@ namespace ScanAction
                                 address += 8;
                             }
                             break;
+                        */
                         case "landtable":
                         case "texlist":
                         case "texnamearray":
@@ -64,61 +78,114 @@ namespace ScanAction
                             break;
                         case "motion":
                         case "animation":
+                            // Pass shape motions through as-is
                             if (item.Value.Filename.Contains(".nas"))
                             {
                                 Console.WriteLine("SHAPE: " + item.Value.Filename);
                                 newdata.Files.Add(item.Key, item.Value);
                                 break;
                             }
-                            List<int> numverts_list = new List<int>();
-                            string objName = null;
-                            int[] numverts = new int[0];
-                            int numparts = 0;
+                            // If there's a "refaddr" field, use the reference model to calculate model parts
                             if (item.Value.CustomProperties.ContainsKey("refaddr"))
                             {
                                 NJS_OBJECT objm = new NJS_OBJECT(datafile, int.Parse(item.Value.CustomProperties["refaddr"], NumberStyles.HexNumber), (uint)iniData.ImageBase, ModelFormat.BasicDX, new Dictionary<int, string>(), new Dictionary<int, Attach>());
-                                objName = objm.Name;
-                                numverts = objm.GetVertexCounts();
-                                if (item.Value.Filename.Contains("shape") || (item.Value.Filename.Contains(".nas.saanim")))
-                                    numparts = objm.CountMorph();
-                                else
-                                    numparts = objm.CountAnimated();
+                                numparts = objm.CountAnimated();
                             }
-                            else
-                            {
-                                if (item.Value.CustomProperties.ContainsKey("numparts"))
-                                    numparts = int.Parse(item.Value.CustomProperties["numparts"]);
-                                if (item.Value.CustomProperties.ContainsKey("numverts"))
-                                {
-                                    string[] vertlist = item.Value.CustomProperties["numverts"].Split(',');
-                                    for (int v = 0; v < vertlist.Length; v++)
-                                    {
-                                        numverts_list.Add(int.Parse(vertlist[v]));
-                                        numverts = numverts_list.ToArray();
-                                    }
-                                }
-                            }
-                            NJS_MOTION mot = new NJS_MOTION(datafile, item.Value.Address, (uint)iniData.ImageBase, numparts, new Dictionary<int, string>(), item.Value.CustomProperties.ContainsKey("shortrot"), numverts);
-                            List<int> actionaddr = FindActions(datafile, (uint)iniData.ImageBase, item.Value);
+                            // Scan for motions: Pass 1
+                            NJS_MOTION mot = new NJS_MOTION(datafile, item.Value.Address, (uint)iniData.ImageBase, numparts, new Dictionary<int, string>(), item.Value.CustomProperties.ContainsKey("shortrot"));
+                            List<int> actionaddr = FindActions(datafile, (uint)iniData.ImageBase, item.Value, chunk);
                             if (actionaddr.Count == 1)
                             {
-                                Console.WriteLine(item.Value.Filename + ": " + actionaddr[0].ToString("X"));
-                                newdata.Files.Add(item.Key, new SplitTools.FileInfo { Type = "action", Address = actionaddr[0], Filename = item.Value.Filename });
+                                try
+                                {
+                                    //Console.WriteLine("Try action at {0} ({1})", actionaddr[0].ToString("X"), ((uint)iniData.ImageBase + actionaddr[0]).ToString("X"));
+                                    NJS_ACTION act = new NJS_ACTION(datafile, actionaddr[0], (uint)iniData.ImageBase, ModelFormat.BasicDX, new Dictionary<int, Attach>());
+                                    Console.WriteLine(item.Value.Filename + ": " + string.Format("{0} ({1})", actionaddr[0].ToString("X"), ((uint)iniData.ImageBase + actionaddr[0]).ToString("X")));
+                                    newdata.Files.Add(item.Key, new SplitTools.FileInfo { Type = "action", Address = actionaddr[0], Filename = item.Value.Filename });
+                                }
+                                catch
+                                {
+                                    Console.WriteLine("ERROR: " + item.Value.Filename);
+                                    newdata.Files.Add(item.Key, item.Value);
+                                }
                             }
                             else if (actionaddr.Count > 1)
                             {
                                 StringBuilder sb = new StringBuilder();
                                 foreach (int act in actionaddr)
                                 {
-                                    sb.Append(act.ToString("X") + ",");
+                                    sb.Append(string.Format("{0} ({1})", act.ToString("X"), ((uint)iniData.ImageBase + act).ToString("X")));
                                 }
                                 newdata.Files.Add(item.Key, new SplitTools.FileInfo { Type = "actionM", Address = actionaddr[0], Filename = item.Value.Filename, CustomProperties = new Dictionary<string, string> { { "addresses", sb.ToString() } } });
                                 Console.WriteLine("MULTIPLE: " + item.Value.Filename + ": " + sb.ToString());
                             }
+                            // Scan for motions: Pass 2
                             else
                             {
-                                Console.WriteLine("NOT FOUND: " + item.Value.Filename);
-                                newdata.Files.Add(item.Key, item.Value);
+                                // Try looking through split items to find a reference model
+                                if (!item.Value.CustomProperties.ContainsKey("refaddr"))
+                                {
+                                    bool found = false;
+                                    foreach (var data in iniData.Files)
+                                    {
+                                        if (found)
+                                            break;
+                                        switch (data.Value.Type)
+                                        {
+                                            case "model":
+                                            case "basicmodel":
+                                            case "basicdxmodel":
+                                                if (data.Value.CustomProperties.ContainsKey("animations") && data.Value.CustomProperties["animations"].Contains(Path.GetFileName(item.Value.Filename)))
+                                                {
+                                                    Console.WriteLine("Found model: " + data.Value.Address.ToString("X") + " (" + ((uint)iniData.ImageBase + data.Value.Address).ToString("X") + ")");
+                                                    chunk = false;
+                                                    found = true;
+                                                    item.Value.CustomProperties.Add("refaddr", data.Value.Address.ToString("X"));
+                                                }
+                                                break;
+                                            case "chunkmodel":
+                                                if (data.Value.CustomProperties.ContainsKey("animations") && data.Value.CustomProperties["animations"].Contains(Path.GetFileName(item.Value.Filename)))
+                                                {
+                                                    chunk = true;
+                                                    found = true;
+                                                    item.Value.CustomProperties.Add("refaddr", data.Value.Address.ToString("X"));
+                                                }
+                                                break;
+                                            default:
+                                                break;
+                                        }
+                                    }
+                                }
+                                actionaddr = FindActions(datafile, (uint)iniData.ImageBase, item.Value, chunk);
+                                if (actionaddr.Count == 1)
+                                {
+                                    try
+                                    {
+                                        NJS_ACTION act = new NJS_ACTION(datafile, actionaddr[0], (uint)iniData.ImageBase, ModelFormat.BasicDX, new Dictionary<int, Attach>());
+                                        Console.WriteLine(item.Value.Filename + ": " + string.Format("{0} ({1})", actionaddr[0].ToString("X"), ((uint)iniData.ImageBase + actionaddr[0]).ToString("X")));
+                                        newdata.Files.Add(item.Key, new SplitTools.FileInfo { Type = "action", Address = actionaddr[0], Filename = item.Value.Filename });
+                                    }
+                                    catch
+                                    {
+                                        Console.WriteLine("ERROR: " + item.Value.Filename);
+                                        newdata.Files.Add(item.Key, item.Value);
+                                    }
+                                }
+                                else if (actionaddr.Count > 1)
+                                {
+                                    StringBuilder sb = new StringBuilder();
+                                    foreach (int act in actionaddr)
+                                    {
+                                        sb.Append(string.Format("UNCONFIRMED MULTIPLE: {0} ({1})", act.ToString("X"), ((uint)iniData.ImageBase + act).ToString("X")));
+                                    }
+                                    newdata.Files.Add(item.Key, new SplitTools.FileInfo { Type = "actionM", Address = actionaddr[0], Filename = item.Value.Filename, CustomProperties = new Dictionary<string, string> { { "addresses", sb.ToString() } } });
+                                    Console.WriteLine("MULTIPLE: " + item.Value.Filename + ": " + sb.ToString());
+                                }
+                                else
+                                {
+                                    Console.WriteLine("NOT FOUND: " + item.Value.Filename + " (" + ((uint)iniData.ImageBase + item.Value.Address).ToString("X") + ")");
+                                    newdata.Files.Add(item.Key, item.Value);
+                                }
                             }
                             break;
                     }
--- CommandLine/ScanAction/ScanAction.cs
+++ CommandLine/ScanAction/ScanAction.cs
@@ -1,68 +1,87 @@
-﻿using SAModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using SAModel;
 
 namespace ScanAction
 {
     partial class Program
     {
-        private static List<int> FindActions(byte[] datafile, uint imageBase, SplitTools.FileInfo fileInfo)
+        private static List<int> FindActions(byte[] datafile, uint imageBase, SplitTools.FileInfo fileInfo, bool chunk)
         {
             List<int> result = new List<int>();
+            // Set motion pointer
             uint motionPointer = imageBase + (uint)fileInfo.Address;
-            uint objectPointer = 0;
-            List<int> numverts_list = new List<int>();
-            int[] numverts = new int[0];
+            // Set number of parts
             int numparts = 0;
-            bool shape = false;
+            if (fileInfo.CustomProperties.ContainsKey("numparts"))
+                numparts = int.Parse(fileInfo.CustomProperties["numparts"]);
+            // Set reference model
+            uint objectPointer = 0;
             if (fileInfo.CustomProperties.ContainsKey("refaddr"))
             {
                 objectPointer = imageBase + uint.Parse(fileInfo.CustomProperties["refaddr"], NumberStyles.HexNumber);
             }
-            else
+            // In most cases the action is right after the motion, so this should be checked first
+            if (BitConverter.ToUInt32(datafile, fileInfo.Address + 0x10) == motionPointer)
             {
-                if (fileInfo.CustomProperties.ContainsKey("numparts"))
-                    numparts = int.Parse(fileInfo.CustomProperties["numparts"]);
-                if (fileInfo.CustomProperties.ContainsKey("numverts"))
+                // If the reference object is known, compare the pointer to it
+                if (objectPointer != 0)
                 {
-                    return result; // Stop on shape motions for now
-                    shape = true;
-                    string[] vertlist = fileInfo.CustomProperties["numverts"].Split(',');
-                    for (int v = 0; v < vertlist.Length; v++)
+                    if (BitConverter.ToUInt32(datafile, fileInfo.Address + 0xC) == objectPointer)
+                        result.Add(fileInfo.Address + 0xC);
+                }
+                // Otherwise read the pointer and check if the object has the same number of animated parts
+                else
+                {
+                    try
                     {
-                        numverts_list.Add(int.Parse(vertlist[v]));
-                        numverts = numverts_list.ToArray();
+                        uint objPointer = BitConverter.ToUInt32(datafile, fileInfo.Address + 0xC);
+                        NJS_OBJECT testobj = new NJS_OBJECT(datafile, (int)(objPointer - imageBase), imageBase, chunk ? ModelFormat.Chunk : ModelFormat.BasicDX, new Dictionary<int, Attach>());
+                        if (testobj.CountAnimated() == numparts)
+                            result.Add(fileInfo.Address + 0xC);
                     }
+                    catch { }
                 }
             }
-            for (int i = 0; i < datafile.Length - 4; i+=4)
+            // If that didn't work, scan the whole file
+            else
             {
-                if (BitConverter.ToUInt32(datafile, i) == motionPointer)
+                if (objectPointer == 0)
                 {
-                    if (objectPointer != 0)
+                    Console.WriteLine("No reference model: " + fileInfo.Filename + " (" + (imageBase + fileInfo.Address).ToString("X") + ")");
+                }
+                // Scan the file
+                for (int i = 0; i < datafile.Length - 4; i += 4)
+                {
+                    // If the motion pointer is detected, try checking if it's an action
+                    if (BitConverter.ToUInt32(datafile, i) == motionPointer)
                     {
-                        if (BitConverter.ToUInt32(datafile, i - 4) == objectPointer)
-                            result.Add(i - 4);
+                        // If the object pointer is known, just check that
+                        if (objectPointer != 0)
+                        {
+                            if (BitConverter.ToUInt32(datafile, i - 4) == objectPointer)
+                                result.Add(i - 4);
+                        }
+                        // Otherwise load the object from the pointer before the motion pointer and compare the number of animated parts
+                        else
+                        {
+                            uint pointer = BitConverter.ToUInt32(datafile, i - 4);
+                            if (pointer == 0 || pointer < imageBase)
+                                continue;
+                            int modeladdr = (int)(pointer - imageBase);
+                            try
+                            {
+                                NJS_OBJECT obj = new NJS_OBJECT(datafile, modeladdr, imageBase, chunk ? ModelFormat.Chunk : ModelFormat.BasicDX, new Dictionary<int, Attach>());
+                                if (obj.CountAnimated() == numparts)
+                                    result.Add(i - 4);
+                            }
+                            catch
+                            {
+                                continue;
+                            }
+                        }
                     }
-                    else
-                    {
-                        uint pointer = BitConverter.ToUInt32(datafile, i - 4);
-                        if (pointer == 0 || pointer < imageBase)
-                            continue;
-                        int modeladdr = (int)(pointer - imageBase);
-                        NJS_OBJECT obj = new NJS_OBJECT(datafile, modeladdr, imageBase, ModelFormat.BasicDX, new Dictionary<int, Attach>());
-                        
-                        if (!shape && obj.CountAnimated() == numparts)
-                            result.Add(i - 4);
-                        else if (shape)
-                            return result; // Stop on shape motions for now
-                        /*
-                        if (shape && obj.GetVertexCounts() == numverts)
-                            result.Add(i - 4);
-                        */
-                    }
-
                 }
             }
             return result;
